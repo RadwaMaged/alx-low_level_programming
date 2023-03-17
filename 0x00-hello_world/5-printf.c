c80fca7f5e15406012a299ba68c7bd9dafe0fa9f
Radwa <radwa.maged921@gmail.com>
2023-03-17 06:50:22 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 /**
  * main - "prints with proper grammar,but the outcome is
  * a piece of art," followed new line
- * Return : Always 0
+ * Return : 0 (Success)
  */
 
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
