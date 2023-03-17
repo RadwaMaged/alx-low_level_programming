9184b5e48c5db89985f406a0559e3970633e7dc9
Radwa <radwa.maged921@gmail.com>
2023-03-17 05:40:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /**
  * main - A program that prints a line with puts function
  * Return: 0 (Succes)
-*/
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzz
-le");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
